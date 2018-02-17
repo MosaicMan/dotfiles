a596f545df158cd556a60ec993e5e17f0f1d9f66
MosaicMan <>
2018-02-17 01:53:22 -0500

    Added editors and desktop utilities to setup script

--- workstation_setup.sh
+++ workstation_setup.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
-sudo apt install software-properties-common python-software-properties curl build-essential dkms -y
+
+# Install Nerd Font
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/Hack.zip
+unzip Hack.zip -d ~/.local/share/fonts/Hack
+rm -f Hack.zip
+
+# Install Base Packages
+sudo apt install software-properties-common python-software-properties curl build-essential dkms gnupg -y
+
+# Add VS Code Repository
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# Add Sublime Text Repository
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# Add Google Chrome Repository
+wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+# Add PPAs
 sudo add-apt-repository ppa:neovim-ppa/stable -y
 sudo add-apt-repository ppa:numix/ppa -y
 sudo add-apt-repository ppa:noobslab/themes -y
 sudo add-apt-repository ppa:noobslab/icons -y
+
 sudo apt update
+
+# Install System Packages
 sudo apt-get install \
 	stow \
 	neovim \
@@ -21,7 +46,9 @@ sudo apt-get install \
 	software-properties-gtk \
 	nodejs \
 	lightdm \
-	unity-greeter \
+	unity-greeter unity-greeter-badges \
+	ubuntustudio-wallpapers \
+	plymouth-theme-ubuntu-budgie-logo \
 	i3 \
 	i3blocks \
 	rofi \
@@ -34,6 +61,10 @@ sudo apt-get install \
 	gnome-terminal \
 	lxappearance \
 	feh \
+	file-roller zip unzip \
+	seahorse seahorse-daemon \
+	keepass2 \
+	mono-xbuild cmake \
 	arc-theme \
 	arc-icons \
 	numix-icon-theme \
@@ -42,10 +73,13 @@ sudo apt-get install \
 	numix-icon-theme-square \
 	firefox \
 	breeze-cursor-theme chameleon-cursor-theme dmz-cursor-theme oxygen-cursor-theme oxygen-cursor-theme-extra xcursor-themes \
-	fonts-ubuntu-font-family-console ttf-ubuntu-font-family edubuntu-fonts \
+	fonts-font-awesome fonts-ubuntu-font-family-console ttf-ubuntu-font-family edubuntu-fonts \
+	gedit gedit-plugins code \
 	-y
-git clone https://github.com/FortAwesome/Font-Awesome
-git clone https://github.com/ryanoasis/nerd-fonts
-mkdir ~/.fonts
+
+# Install OhMyZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Turn off git pager
+sh -c "git config --global --replace-all core.pager cat"
 
